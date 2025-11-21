4fde7ee1ce04bcc744f3e24999e3b8ac36fab876
Mike Westaway <mike.westaway@microsoft.com>
2025-11-21 17:03:25 +0000

    test harness

---
+++ testWaitForLogsAndIngest.ps1
@@ -0,0 +1,11 @@
+& ./waitForLogsAndIngest.ps1 `
+    -workspaceId b5b2874f-f4fb-4581-b606-af3b15af8fea `
+    -computerName LAPTOP-JF9KNPOJ `
+    -sourceLogFile C:\ProgramData\AzureConnectedMachineAgent\Log\arcproxy*.log `
+    -targetTable LinuxTextLogs2_CL `
+    -dcrImmutableId dcr-d14c760418244661b683391a0022de18 `
+    -endpointUri https://arc-servers-uks-endpoint-5lkb.uksouth-1.ingest.monitor.azure.com `
+    -timestampColumn TimeGenerated `
+    -timeSpan P1D `
+    -isArcConnectedMachine true
+    
