3a1cfbd4c0a9f9f541c84e78e2a99d130fa31c85
Mike Westaway <mike.westaway@microsoft.com>
2025-11-28 12:55:09 +0000

    Now works for Arc and Azure

--- assignConnectedMachineSystemManagedIdentityRoles.ps1
+++ assignConnectedMachineSystemManagedIdentityRoles.ps1
@@ -4,12 +4,22 @@ $ResourceGroupConnectedMachine = "<your-resource-group>"
 $ConnectedMachineName = "<your-connected-machine-name>"
 $SubscriptionId = "<your-subscription>"
 $Scope = "/subscriptions/$SubscriptionId/resourceGroups/$ResourceGroupDcr"  # Change scope if needed
+$IsArc = $false
 
 # 1. Get the principalId of the system-assigned managed identity
-$PrincipalId = az connectedmachine show `
-    --name $ConnectedMachineName `
-    --resource-group $ResourceGroupConnectedMachine `
-    --query "identity.principalId" -o tsv
+
+if ($IsArc -eq $true) {
+    $PrincipalId = az connectedmachine show `
+        --name $ConnectedMachineName `
+        --resource-group $ResourceGroupConnectedMachine `
+        --query "identity.principalId" -o tsv
+} else {
+    $PrincipalId = az vm show `
+        --name $ConnectedMachineName `
+        --resource-group $ResourceGroupConnectedMachine `
+        --query "identity.principalId" -o tsv
+}
+
 Write-Host "Managed Identity Principal ID: $PrincipalId"
 # 2. Assign roles to the managed identity
 $Roles = @(
